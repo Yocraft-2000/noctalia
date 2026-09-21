6b037bcaa981fe5dada3383a51f691ee448ba311
Yocraft-2000 <304616174+Yocraft-2000@users.noreply.github.com>
2026-09-21 17:34:15 +0200

    feat(shell): close overview when launching an app from the launcher

--- src/config/config_types.h
+++ src/config/config_types.h
@@ -1102,6 +1102,8 @@ struct ShellConfig {
   bool setupWizardEnabled = true;
   bool niriOverviewTypeToLaunchEnabled = false;
   bool umbrielOverviewTypeToLaunchEnabled = false;
+  bool niriOverviewCloseOnLaunchEnabled = false;
+  bool umbrielOverviewCloseOnLaunchEnabled = false;
   bool polkitAgent = false;
   PasswordMaskStyle passwordMaskStyle = PasswordMaskStyle::CircleFilled;
   AnimationConfig animation;
